ef79180724c227c04b62fb5fa8be070d1e1e2dd2
SalahShadoud <69741468+SalahShadoud@users.noreply.github.com>
2023-08-21 13:41:32 -0700

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,6 @@
 # Build the project
 echo "Building the project..."
 pip install Django
-pip freeze
 echo "Make Migration..."
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
